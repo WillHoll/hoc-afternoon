a2cb533d1815b44f3d3d46d73d5d6d9bfe055ff4
WillHoll <willhollingsworth15@yahoo.com>
2019-11-13 15:11:32 -0700

    step 3 complete

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
-class Currency extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
+const withCurrency = (BaseComponent) => (
+  class Currency extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        currencyChosen: false,
+        selectedCurrency: 'Select Currency',
+        amount: 0
+      }
+    }
+
+    render() {
+      const currencyData = [
+        { name: 'Japanese Yen', symbol: '¥', rate: 113.6, id: 0 },
+        { name: 'British Pound', symbol: '£', rate: 0.77, id: 1 },
+        { name: 'Canadian Dollar', symbol: 'CAD', rate: 1.32, id: 2 },
+        { name: 'Mexican Peso', symbol: 'Mex$', rate: 20.41, id: 3 },
+        { name: 'Swiss Franc', symbol: 'Fr.', rate: 1.01, id: 4 }
+      ]
+      const currencyOptions = currencyData.map((el, i) => {
+        <option key={i} value={i}>{el.name}</option>
+      })
+      return (
+        <div>
+          <select>
+            <option value='Select Currency'>Select Currency</option>
+            {currencyOptions}
+          </select>
+          <div>
+            <button className='add'>+</button>
+            <button className='subtract'>-</button>
+            {this.props.render(currencyData[this.state.selectedCurrency], this.state.amount)}
+          </div>
+        </div>
+      );
+    }
   }
-}
+)
 
 export default Currency;
