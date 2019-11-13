e7fc4e535f839c11f4b59c134bd69b77db73ffcc
WillHoll <willhollingsworth15@yahoo.com>
2019-11-13 14:43:52 -0700

    step 2 cmoplete

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+class Currency extends Component {
   render() {
     return (
       <div>
-
+        
       </div>
     );
   }
 }
 
-export default App;
+export default Currency;
